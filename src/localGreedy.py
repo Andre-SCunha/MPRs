51e3f8f14d3e71217083d1c78195a39788fb3519
Andre-SCunha <brayils.andre@gmail.com>
2017-12-04 23:14:27 -0200

    Results localGreedy

--- src/localGreedy.py
+++ src/localGreedy.py
@@ -2,7 +2,6 @@ import sys
 import ast
 
 Graph = {}
-MPRs = {}
 
 def readingGraph():
     with open(sys.argv[1], 'r') as f:
@@ -10,36 +9,53 @@ def readingGraph():
         global Graph
         Graph = ast.literal_eval(s)
 
-def calcEssential(S):
+def calcEssential(N1):
     dic = {}
-    for n in S:
-        for k in Graph[n]:
-            dic[k] = 0
-    for n in S:
-        for k in Graph[n]:
-            dic[k] += 1
+    for n in N1:
+        for n2 in Graph[n]:
+            dic[n2] = 0
+    for n in N1:
+        for n2 in Graph[n]:
+            dic[n2] += 1
     List = []
     for i in dic:
-        if dic[i] == 1 and i not in S:
+        if dic[i] == 1 and i not in N1:
             List.append(i)
     return List
 
 def calcMPR(node):
-    neig = set(Graph[node])
-    S = []
-    U = set([])
+    MPR = set()
+    reach = set()
+    neig = Graph[node]
+    neig2 = set()
     for n in neig:
-        t = (n, [])
-        for n2 in Graph[n]:
-            if n2 not in neig and n2 != node:
-                t[1].append(n2)
-                U.add(n2)
-        S.append(t)
-    S = sorted(S, key = lambda t: -len(t[1]))
-    return setCover(S,U)[0]
+        neig2.update(Graph[n])
+    neig2.difference_update(neig)
+    reach.update(calcEssential(neig))
+    for n in neig:
+        if (len(set(Graph[n])&reach)>0):
+            MPR.add(n)
+    for n in MPR:
+        reach.update(Graph[n])
+    left = set(neig) - MPR
+    left2 = (neig2 - reach)
+    while(len(left2) > 0):
+        best = None
+        bestlen = -1
+        for n in left:
+            ln = len(set(Graph[n])&(left2))
+            if(ln > bestlen):
+                best = n
+                bestlen = ln
+            elif(ln == bestlen and len(set(Graph[n]) - left2) < len(set(Graph[best]) - left2)):
+                best = n
+        MPR.add(best)
+        left.remove(best)
+        left2.difference_update(Graph[best])
+    return MPR
 
 
 
 readingGraph()
-print calcEssential(Graph["a"])
-#print calcMPR(0)
+#print calcEssential(Graph["a"])
+print calcMPR(0)
